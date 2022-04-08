8078b97002a350e4cabd5b6b63db3925b87e5198
nmttuan <nmttuan17@gmail.com>
2022-04-09 00:54:00 +0700

    added factorize function

---
+++ factorize.py
@@ -0,0 +1,24 @@
+def factorize(n: int, num: dict = None) -> dict:
+    '''Factorize a number into its prime factors.
+    Return a dictionary of prime factors and their multiplicity.
+    Arguments:
+        n: the number to factorize
+        num: a dictionary of prime factors and their multiplicity'''
+    if num == None:
+        num = {}
+    if n == 1:
+        return num
+    for i in (range(list(num.keys())[-1], n + 1) if len(num) > 0 else range(2, n + 1)):
+        if i * i > n:
+            num[n] = num[n] + 1 if n in num else 1
+            return num
+        if n % i == 0:
+            num[i] = num[i] + 1 if i in num else 1
+            return factorize(n // i, num)
+
+def main():
+    '''Main function'''
+    print(factorize(int(input('Enter a number: '))))
+
+if __name__ == '__main__':
+    main()
